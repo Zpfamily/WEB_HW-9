deaf6999d5596301b58fb84ce0ddef4241e26c19
Zpfamily <65131398+Zpfamily@users.noreply.github.com>
2024-03-04 17:54:23 +0200

    Update scrap.py
    Add to author - born_date, location and description

--- scrap.py
+++ scrap.py
@@ -6,6 +6,7 @@ import os
 
 def scrape_quotes(base_url):
     quotes = []
+    authors = []
     url = base_url
     while url:
         response = requests.get(url)
@@ -15,13 +16,18 @@ def scrape_quotes(base_url):
             author = quote.find(class_="author").get_text()
             tags = [tag.get_text() for tag in quote.find_all(class_="tag")]
             quotes.append({"quote": text, "author": author, "tags": tags})
+            # Scrape author's birth date, place, and description
+            author_page_url = base_url + 'author/' + author.replace(" ", "-")
+            author_response = requests.get(author_page_url)
+            author_soup = BeautifulSoup(author_response.text, "html.parser")
+            birth_date = author_soup.find("span", class_="author-born-date").get_text()
+            birth_place = author_soup.find("span", class_="author-born-location").get_text()
+            description = author_soup.find("div", class_="author-description").get_text().strip()
+            author_info = {"fullname": author, "born_date": birth_date, "born_location": birth_place, "description": description}
+            authors.append(author_info)
         next_button = soup.find(class_="next")
-        url = next_button.find("a")["href"] if next_button else None
-        # If 'next_button' exists, it contains a relative URL, so we need to join it with the base URL
-        if url and not url.startswith("http"):
-            url = base_url + url
-    return quotes
-
+        url = base_url + next_button.find("a")["href"] if next_button else None
+    return quotes, authors
 
 
 def save_to_json(data, filename):
@@ -31,16 +37,14 @@ def save_to_json(data, filename):
 
 def main():
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    quotes = scrape_quotes("http://quotes.toscrape.com/")
+    quotes, authors = scrape_quotes("http://quotes.toscrape.com/")
+    
     quotes_filename = os.path.join(current_directory, "quotes.json")
     save_to_json(quotes, quotes_filename)
 
-    # Створення файлу з інформацією про авторів
-    authors = [{"fullname": quote["author"]} for quote in quotes]
     authors_filename = os.path.join(current_directory, "authors.json")
     save_to_json(authors, authors_filename)
-    
+
+
 if __name__ == "__main__":
     main()
-
-    
